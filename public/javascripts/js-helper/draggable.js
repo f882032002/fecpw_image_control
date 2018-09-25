da448a07fcd54eafd87ab5f37b89527640434420
fullcorp <>
2018-09-25 17:38:35 +0800

    整理資料

--- public/javascripts/js-helper/draggable.js
+++ public/javascripts/js-helper/draggable.js
@@ -1,26 +1,4 @@
 
-/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ SVG add ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
-
-function makeSVG(tag, attrs) {
-  var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
-  for (var k in attrs)
-      el.setAttribute(k, attrs[k]);
-  return el ;
-}
-
-/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ jQuery ui Droppable (Map) ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
-
-$(() => {
-  let svg_map = $('#svg')
-  svg_map.droppable({
-    accept: '.item',
-    drop: function (event, ui) {
-      deleteItem(ui.draggable)
-    }
-  })
-
-})
-
 /* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ Draggable Start (Devices) ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
 function dra_start (x){
@@ -31,29 +9,6 @@ function dra_start (x){
     })
   $('#map_img').css('background-color', 'rgba(255,192,203,0.7)')
 }
-
-
-/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ 轉換成 SVG X,Y ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
-
-function getPosition (a){
-
-  const clientPoint = svg.createSVGPoint()
-  const CTM = svg.getScreenCTM()
-
-  let z =  
-  
-  clientPoint.x = a.clientX
-  clientPoint.y = a.clientY
-  SVGPoint = clientPoint.matrixTransform(CTM.inverse())
-
-  console.log( 
-    Math.round(SVGPoint.x - 15),
-    Math.round(SVGPoint.y - 15)
-  )
-  return z
-}
-
-
 
 /* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ jQuery ui Draggable (Devices) ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
@@ -77,81 +32,26 @@ $(() => {
     },
 
     stop: function (event, ui) {   // 拖動停止後
-
+      
       getPosition (event)
       let name = ui.helper[0].attributes.name.value
       let _x = Math.round(SVGPoint.x - 15)
       let _y = Math.round(SVGPoint.y - 15)
-      let _img; // 放入 image
-
+      console.log(_x,_y)
       // 用 name 判斷並抓出圖片
-
-      switch (name) {
-        case 'idc-gateway':
-          _img = icon.gateway;
-          break;
-        case 'ids-co':
-          _img = icon.co;
-          break;
-        case 'ids-co2':
-          _img = icon.co2;
-          break;
-        case 'ids-door':
-          _img = icon.door;
-          break;
-        case 'idc-light-rgb':
-          _img = icon.light;
-          break;
-        case 'idc-plug':
-          _img = icon.socket;
-          break;
-        case 'ids-thermo':
-          _img = icon.thermometer;
-          break;
-        case 'ids-smoke':
-          _img = icon.thermometer;
-          break;
-        case 'ids-gas':
-          _img = icon.smoke;
-          break;
-        case 'ids-glass':
-          _img = icon.thermometer;
-          break;
-        case 'ids-pir':
-          _img = icon.pir;
-          break;
-        case 'ids-water':
-          _img = icon.water;
-          break;
-        case 'idc-sos':
-          _img = icon.thermometer;
-          break;
-        case 'ids-light':
-          _img = icon.thermometer;
-          break;
-        case 'idc-door-lock':
-          _img = icon.door_lock;
-          break;
-        case 'idc-q-module':
-          _img = icon.q_module;
-          break;
-        case 'idc-water-valve':
-          _img = icon.thermometer;
-          break;
-        case 'idc-gas-keeper':
-          _img = icon.thermometer;
-          break;
-      };
-
+      image_switch(name)
+      
       var _image = makeSVG('image',{
         class: 'device_icon',
         x : _x, 
         y : _y,
         width: 30, 
         height: 30, 
-        href: _img
+        href: _icon
       })
+
       $(_image).appendTo('#svg');
+      event.target.remove()
       moveDev();
       $('#map_img').css('background-color', 'rgba(255,192,203,0.3)');
     },
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,4 @@
+
 /* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ MinWidth ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
 let window_h = $(window).height();
@@ -15,54 +16,19 @@ $(document).ready(() => {
   })
 });
 
-/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ Colors ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
-let Toilet_color = '#93E6CE'
-let LivingRoom_color = '#93CEE6'
-let BedRoom_color = '#CEBEEB'
+/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ SVG add ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
-
-/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ data ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
-
-let data = {
-  maps: [ // 地圖們
-    { // 一樓平面圖
-      name: '1F',
-      map_url: '/images_map/1F.png',
-      areas: [{ // 一樓廁所
-          name: 'Toilet',
-          x: 0,
-          y: 0,
-          _id: 'ta001',
-          value: 'toilet',
-          point: '',
-          color: Toilet_color,
-          devices: [ /* 一樓廁所裡的設備 */ ]
-        },
-        { // 一樓房間
-          name: 'BedRoom',
-          x: 0,
-          y: 0,
-          _id: 'ba001',
-          value: 'bedroom',
-          color: BedRoom_color,
-          point: '',
-          devices: [ /* 一樓房間裡的設備 */ ]
-        },
-        { // 一樓客廳
-          name: 'LivingRoom',
-          x: 0,
-          y: 0,
-          _id: 'la001',
-          value: 'livingRoom',
-          color: LivingRoom_color,
-          point: '',
-          devices: [ /* 一樓客廳裡的設備 */ ]
-        }
-      ]
-    }
-  ]
+function makeSVG(tag, attrs) {
+  var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
+  for (var k in attrs)
+      el.setAttribute(k, attrs[k]);
+  return el ;
 }
+
+
+
+
 
 
 /* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ Button Object Data ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
@@ -90,6 +56,13 @@ function myDevice(a, b, c) {
       </li>  
     `
 }
+/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ Device Image On Map Html Template ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
+
+function myDeviceOnMap(a, b, c) {
+  return `
+  <image class="device_icon" x="${a}" y="${b}" width="30" height="30" href="${c}"></image>
+  `
+}
 
 /* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ Area li Html Template ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
@@ -108,49 +81,52 @@ function myMap(d) {
   return `
     <svg width="1900" height="1000" id="svg" viewBox="0 0 1900 1000" xmlns="http://www.w3.org/1999/xhtml" >
       <image x="0" y="0" width="1900" height="1000" href="${d}"></image>
-      <rect 
-      x="50" y="50" rx="20" ry="20"
-      fill="rgba(101, 168, 166, 0.5)" 
-      width="250" 
-      height="250" 
-      />
-      <image class="device_icon" x="950" y="500" width="30" height="30" href="${icon.co}"></image>
-      <image class="device_icon" x="1220" y="731" width="30" height="30" href="${icon.light}"></image>
-      <image class="device_icon" x="723" y="151" width="30" height="30" href="${icon.door_lock}"></image>
-      <image class="device_icon" x="1159" y="137" width="30" height="30" href="${icon.door}"></image>
-      <image class="device_icon" x="632" y="678" width="30" height="30" href="${icon.water}"></image>
-      <image class="device_icon" x="1224" y="198" width="30" height="30" href="${icon.thermometer}"></image>
     </svg>
   `
 }
 
+/* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ 轉換成 SVG X,Y ( CTM 轉換器) ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
+function getPosition (a){
 
+  const clientPoint = svg.createSVGPoint()
+  const CTM = svg.getScreenCTM()
+
+  let z =  
+  
+  clientPoint.x = a.clientX
+  clientPoint.y = a.clientY
+  SVGPoint = clientPoint.matrixTransform(CTM.inverse())
+
+  // console.log( 
+  //   Math.round(SVGPoint.x - 15),
+  //   Math.round(SVGPoint.y - 15)
+  // )
+  return z
+}
 
 
 /* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ Device On Map Move It ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
 function moveDev() {
   //  點擊坐標 ！！！
-  let dx, dy, ux, uy, mx, my, dev_onMapX, dev_onMapY
-  let svg = $('#svg')
-  let dev_onMap = svg.find('.device_icon')
+  let svg         = $('#svg')
+  let dev_onMap   = svg.find('.device_icon')
   let isMouseDown = false
-  let dev_onMap_w = dev_onMap.width()
-  let dev_onMap_h = dev_onMap.height()
 
   function moveNow() {
     if (isMouseDown === true) {
 
       dev_onMap.on('mousemove', function (e) {
 
-        mx = e.offsetX;
-        my = e.offsetY;
+        getPosition (e)
+        
         $(this).attr({
-          'x': mx - (dev_onMap_w / 2),
-          'y': my - (dev_onMap_h / 2)
+          'x': Math.round(SVGPoint.x - 15),
+          'y': Math.round(SVGPoint.y - 15)
         })
       })
+
     } else {
       dev_onMap.off('mousemove')
     }
@@ -159,10 +135,7 @@ function moveDev() {
   dev_onMap.on('mousedown', function (e) {
 
     isMouseDown = true
-    dx = e.offsetX;
-    dy = e.offsetY;
-    dev_onMapX = $(this).attr('x')
-    dev_onMapY = $(this).attr('y')
+    getPosition (e)
     moveNow()
     
   });
@@ -170,13 +143,13 @@ function moveDev() {
   dev_onMap.on('mouseup', function (e) {
 
     isMouseDown = false
-    ux = e.offsetX;
-    uy = e.offsetY;
+    getPosition (e)
     $(this).attr({
-      'x': ux - (dev_onMap_w / 2),
-      'y': uy - (dev_onMap_h / 2)
+      'x': Math.round(SVGPoint.x - 15),
+      'y': Math.round(SVGPoint.y - 15)
     })
     moveNow()
+
   })
 }
 
@@ -186,22 +159,16 @@ function moveDev() {
 
 function moveArea() {
   //  點擊坐標 ！！！
-  let dx, dy, ux, uy, mx, my, rectX, rectY
-  let svg = $('#svg')
-  let rect = svg.find('rect')
+  let svg         = $('#svg')
+  let rect        = svg.find('rect')
   let isMouseDown = false
-  let rect_w = rect.width()
-  let rect_h = rect.height()
 
   rect.on('mousedown', function (e) {
 
     isMouseDown = true
-    dx = e.offsetX;
-    dy = e.offsetY;
-    rectX = $(this).attr('x')
-    rectY = $(this).attr('y')
-    //console.log($(this), rectX, rectY, dx, dy, isMouseDown) // 一開始滑鼠點下去的坐標
+    getPosition (e)
     moveNow()
+
   });
 
   function moveNow() {
@@ -209,12 +176,13 @@ function moveArea() {
 
       rect.on('mousemove', function (e) {
 
-        mx = e.offsetX;
-        my = e.offsetY;
+        getPosition (e)
+        
         $(this).attr({
-          'x': mx - (rect_w / 2),
-          'y': my - (rect_h / 2)
+          'x': Math.round(SVGPoint.x - 125),
+          'y': Math.round(SVGPoint.y - 125)
         })
+
       })
     } else {
       rect.off('mousemove')
@@ -224,13 +192,13 @@ function moveArea() {
   rect.on('mouseup', function (e) {
 
     isMouseDown = false
-    ux = e.offsetX;
-    uy = e.offsetY;
+    getPosition (e)
     $(this).attr({
-      'x': ux - (rect_w / 2),
-      'y': uy - (rect_h / 2)
+      'x': Math.round(SVGPoint.x - 125),
+      'y': Math.round(SVGPoint.y - 125)
     })
     moveNow()
+
   })
 }
 
@@ -274,75 +242,28 @@ function showList() {
     })
   })
 
+
   response.forEach((device, deviceID) => { // 設備呼叫
 
     device.list.forEach((imgs, imgsID) => {
 
-      let _icon; // 放入 image
+      image_switch(imgs.value)
 
-      switch (imgs.value) {
-        case 'idc-gateway':
-          _icon = icon.gateway;
-          break;
-        case 'ids-co':
-          _icon = icon.co;
-          break;
-        case 'ids-co2':
-          _icon = icon.co2;
-          break;
-        case 'ids-door':
-          _icon = icon.door;
-          break;
-        case 'idc-light-rgb':
-          _icon = icon.light;
-          break;
-        case 'idc-plug':
-          _icon = icon.socket;
-          break;
-        case 'ids-thermo':
-          _icon = icon.thermometer;
-          break;
-        case 'ids-smoke':
-          _icon = icon.thermometer;
-          break;
-        case 'ids-gas':
-          _icon = icon.smoke;
-          break;
-        case 'ids-glass':
-          _icon = icon.thermometer;
-          break;
-        case 'ids-pir':
-          _icon = icon.pir;
-          break;
-        case 'ids-water':
-          _icon = icon.water;
-          break;
-        case 'idc-sos':
-          _icon = icon.thermometer;
-          break;
-        case 'ids-light':
-          _icon = icon.thermometer;
-          break;
-        case 'idc-door-lock':
-          _icon = icon.door_lock;
-          break;
-        case 'idc-q-module':
-          _icon = icon.q_module;
-          break;
-        case 'idc-water-valve':
-          _icon = icon.thermometer;
-          break;
-        case 'idc-gas-keeper':
-          _icon = icon.thermometer;
-          break;
-      };
+      let _devicesMap = makeSVG('image',{
+        class: 'device_icon',
+        x : device.x, 
+        y : device.y,
+        width: 30, 
+        height: 30, 
+        href: _icon
+      })
 
       let deviceHtml = {
         view: myDevice(device.name, _icon, imgs.value),
-        key: device._id,
+        key : device._id,
         self: response
       }
-
+      $(_devicesMap)    .appendTo('#svg');
       $(del(deviceHtml)).appendTo('.items')
     })
 
@@ -357,13 +278,13 @@ moveDev();
 /* ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ 縮放按鈕 ▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼ */
 
 $(() => {
-  let _width = 1900; // 初始寬度
+  let _width  = 1900; // 初始寬度
   let _height = 1000; // 初始高度
-  let opa = 0;
+  let opa     = 0;
 
   function init() {
     $('#svg').css({
-      'width': _width,
+      'width' : _width,
       'height': _height
     })
     $('.homer').css('opacity', opa)
@@ -375,9 +296,9 @@ $(() => {
     (_width == 5700, _height == 3000) ? (
       console.log('Has been max width !!')
     ) : (
-      (_width += 190),
+      (_width  += 190),
       (_height += 100),
-      (opa += 0.033),
+      (opa     += 0.033),
       init()
     );
   });
@@ -386,9 +307,9 @@ $(() => {
     (_width == 1900, _height == 1000) ? (
       console.log('Has been min width !!')
     ) : (
-      (_width -= 190),
+      (_width  -= 190),
       (_height -= 100),
-      (opa -= 0.033),
+      (opa     -= 0.033),
       init()
     );
   });
